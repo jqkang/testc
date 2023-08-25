4dab932ac69426880bc19898a35215b02585fcd5
jqkang <eks27jqkang@163.com>
2023-08-25 20:42:38 +0800

    mmodify2

--- testc.c
+++ testc.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
 int main(){
     printf("hello! a new repository in cy's home.");
-    printf("hello! a new repository in cy's home.");
+    printf("hello! modify1 in cy's home.");
+    printf("hello! modify2 in cy's home.");
 
     return 0;
 }
