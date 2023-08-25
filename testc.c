79eb1a473289051e0df84bca9d1fde76443fda3d
jqkang <eks27jqkang@163.com>
2023-08-25 20:56:41 +0800

    modify6

--- testc.c
+++ testc.c
@@ -2,6 +2,7 @@
 int main(){
     printf("hello! a new repository in cy's home.");
     printf("hello! a new repository in cy's home.");
+    printf("hello! a new repository in cy's home.");
 
     return 0;
 }
